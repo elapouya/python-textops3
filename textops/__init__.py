a0abec1ae40759363ff6538a70075d1482843412
Eric Lapouyade <elapouya@gmail.com>
2015-04-06 12:34:00 +0200

    dev

--- textops/__init__.py
+++ textops/__init__.py
@@ -12,36 +12,57 @@ import sys
 import re
 
 class TextOp(object):
+    
     def __init__(self,*args,**kwargs):
         self.ops = [(self.__class__.__name__, args, kwargs)]
-        self.op = None        
+        self.op = None
+        self.text = args and args[0] or ''
+        print self.ops[0]
     
     def __getattr__(self,attr):
-        print 'op =',attr
-        self.op = attr
+        if not attr.startswith('_'):
+            print 'op =',attr
+            self.op = attr
+        else:
+            raise AttributeError()
         return self
     
     def __ror__(self,text):
-        return self._process(text)
+        self.text = text
+        return self._process()
     
     def __call__(self,*args,**kwargs):
-        if self.op:            
+        if self.op:
             print 'op param =',args,kwargs
             self.ops.append((self.op, args, kwargs))
             self.op = None
             return self
         else:
-            return self._process(args[0])
+            if args or kwargs:
+                self.ops[0][1] = args
+                self.ops[0][2] = kwargs
+            self._process()
+            return self.text
         
-    def _process(self,text):
+    @staticmethod    
+    def op(text,*args,**kwargs):
+        return text
+        
+    def _process(self):
         print 'processing...'
         for op,args,kwargs in self.ops:
-            text = getattr(text, op)(*args,**args)
-        return text    
+            print '%%%',op,args,kwargs
+            opcls = globals().get(op)
+            print '°°°',opcls
+            if issubclass(opcls, TextOp):
+                self.text = opcls.op(self.text, *args, **kwargs)
+            else:
+                self.text = getattr(self.text, op)(*args,**kwargs)
 
     def __iter__(self):
         print '__iter__'
-        return iter(['my iter'])
+        self._process()
+        return iter(self.text)
     
     def __unicode__(self):
         print '__unicode__'
@@ -49,12 +70,14 @@ class TextOp(object):
         
     def __str__(self):
         print '__str__'
-        return '__str__'    
+        self._process()
+        return self.text    
         
     def __int__(self):
         print '__int__'
-        return '__int__'    
-        
+        self._process()
+        return int(self.text)
+
     def __repr__(self):
         print '__repr__'
         return '__repr__'    
@@ -85,4 +108,12 @@ class TextOp(object):
         
     def __len__(self):
         print '__len__'
-        return 23    
+        self._process()
+        return len(self.text)    
+
+class length(TextOp):    
+    @staticmethod    
+    def op(text,*args,**kwargs):
+        print '*** length'
+        return len(text)
+    
