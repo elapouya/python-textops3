1bda205a88bc336dc9803f38e24d7d58b2e472e8
Eric Lapouyade <elapouya@gmail.com>
2015-09-24 15:05:54 +0200

    fix import

--- textops/__init__.py
+++ textops/__init__.py
@@ -18,8 +18,8 @@ except ImportError:
 logger = logging.getLogger(__name__)
 logger.addHandler(NullHandler())
 
-from base import TextOp, WrapOpIter, WrapOpYield, register_op, register_op_iter, \
-                 register_op_yield, StrExt, UnicodeExt, ListExt, DictExt, NoAttrDict, NoAttr, \
+from base import TextOp, WrapOpIter, WrapOpYield, add_textop, add_textop_iter, \
+                 add_textop_yield, StrExt, UnicodeExt, ListExt, DictExt, NoAttrDict, NoAttr, \
                  activate_debug
 import ops
 from ops import *
